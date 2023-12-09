ed24f5151ff9be4c862b068927f7a865c32ad31e
rgallardj <74204276+rgallardj@users.noreply.github.com>
2023-12-09 19:21:14 +0100

    Delete gestion_horario directory

--- gestion_horario/src/main/java/es/unex/aos/gestion_horario/repository/HorarioRepository.java
+++
@@ -1,9 +0,0 @@
-package es.unex.aos.gestion_horario.repository;
-
-import es.unex.aos.gestion_horario.model.Horario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HorarioRepository extends CrudRepository<Horario, Long> {
-    
-}
-
