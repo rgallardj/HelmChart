ca84c80bcbe5c3308cedfe268220cf994c2a6f21
rgallardj <74204276+rgallardj@users.noreply.github.com>
2023-12-09 19:20:18 +0100

    Delete gestion_pedido directory

--- gestion_pedido/src/main/java/es/unex/aos/gestion_pedido/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package es.unex.aos.gestion_pedido.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import es.unex.aos.gestion_pedido.model.Pedido;
-
-public interface PedidoRepository extends CrudRepository<Pedido, Long> {
-    
-}
